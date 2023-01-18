5889685916f6d3dde64f06213df6d17e8b4d61bf
Nikita <nikita.khadak.980@gmail.com>
2023-01-18 11:38:00 +0800

    some changes

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const button=document.querySelectorAll('.button');
-const display=document.querySelector('.display')
-const remove=document.querySelector('.remove')
+const display=document.querySelector('.display');
+const remove=document.querySelector('.remove');
+let count=0;
 
 
 button.forEach((i)=>{
     i.addEventListener('click',()=>{
-        display.innerText=display.innerText+i.innerText
-        console.log(i.innerText)
+        if (count<10){
+
+            display.innerText=display.innerText+i.innerText;
+            console.log(i.innerText);
+            count++;
+        }
+        else{
+            alert('Already 10 characters!!')
+        }
 
     })
 
 })
 
 remove.addEventListener('click',()=>{
-    let str=display.innerText;
-    str = str.slice(0, -1); 
-    display.innerText=str
+    if (count==0){
+    alert('No characters to remove')    
+    }
+    else{
+
+        let str=display.innerText;
+        str = str.slice(0, -1); 
+        display.innerText=str;
+        count--;
+    }
 
 })
 
